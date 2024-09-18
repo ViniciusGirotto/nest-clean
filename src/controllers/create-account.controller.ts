d2ec5fe2d5f92e0b3964a3dc195d160beae9f591
Vinicius Girotto <vinicius.s.carvalho23@gmail.com>
2024-09-18 11:25:05 -0300

    Refactor package.json to add zod and zod-validation-error dependencies

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,15 +1,24 @@
-import { ConflictException } from "@nestjs/common";
+import { ConflictException, UsePipes } from "@nestjs/common";
 import { Body, Controller, Post } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { hash } from "bcryptjs";
+import { z } from "zod";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+
+const createAccountBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+})
+
+type CreateAccountBody = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
-
     constructor(private prisma: PrismaService){} 
-
     @Post()
-    async handle(@Body() body: {name: string, email: string, password: string}){
+    @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+    async handle(@Body() body: CreateAccountBody){
         const {name, email, password} = body
 
         const userWithEmailAlreadyExists = await this.prisma.user.findUnique({
