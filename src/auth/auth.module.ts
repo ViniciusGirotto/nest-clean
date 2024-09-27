ce7a331e301f5504fa34b61b0f3ecc9d16f42907
ViniciusGirotto <vinicius.s.carvalho23@gmail.com>
2024-09-26 23:56:02 -0300

    feat: protecting routes with jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { Env } from "src/env";
+import { JwtStrategy } from "./jwt.strategy";
+
 
 @Module({
   imports: [
@@ -14,12 +16,13 @@ import { Env } from "src/env";
         const privateKey = config.get("JWT_PRIVATE_KEY");
         const publicKey = config.get("JWT_PUBLIC_KEY");
         return {
-          signOptions: {algorithm: "RS256"},
+          signOptions: { algorithm: "RS256" },
           privateKey: Buffer.from(privateKey, "base64"),
           publicKey: Buffer.from(publicKey, "base64"),
         };
       },
     }),
   ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
